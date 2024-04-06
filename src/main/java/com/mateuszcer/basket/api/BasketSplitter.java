85db6a49cd40c1d908ddaa915a947340b28bc26e
mateuszcer <cerulewicz.mateusz@gmail.com>
2024-04-06 23:36:00 +0200

    initial setup

---
+++ src/main/java/com/mateuszcer/basket/api/BasketSplitter.java
@@ -0,0 +1,16 @@
+package com.mateuszcer.basket.api;
+
+import java.util.List;
+import java.util.Map;
+
+public class BasketSplitter {
+    /* ... */
+    public BasketSplitter(String absolutePathToConfigFile) {
+        /* ... */
+    }
+
+    public Map<String, List<String>> split(List<String> items) {
+        return Map.of();
+    }
+    /* ... */
+}

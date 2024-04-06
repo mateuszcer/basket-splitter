c6057ab55c23e08aebeba491c7a62c9780da09d6
mateuszcer <cerulewicz.mateusz@gmail.com>
2024-04-07 00:31:55 +0200

    Add FileParser

---
+++ src/main/java/com/mateuszcer/basket/utils/io/FileParser.java
@@ -0,0 +1,33 @@
+package com.mateuszcer.basket.utils.io;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
+import com.mateuszcer.basket.utils.io.exception.FileParserException;
+
+public class FileParser {
+
+    /**
+     * Loads data from a JSON file.
+     *
+     * @param <T>        The type of the object to be returned.
+     * @param filePath The path to the JSON file.
+     * @return The object loaded from the JSON file
+     * @throws FileParserException If an error occurs during file parsing.
+     */
+    public <T> T loadFromJSONFile(String filePath) {
+        Gson gson = new Gson();
+        try (FileReader reader = new FileReader(filePath)) {
+            Type type = new TypeToken<T>() {}.getType();
+            return gson.fromJson(reader, type);
+        } catch (IOException e) {
+            throw new FileParserException("Error reading JSON file: " + e.getMessage(), e);
+        } catch (JsonSyntaxException e) {
+            throw new FileParserException("Error parsing JSON: " + e.getMessage(), e);
+        }
+    }
+}
---
+++ src/test/java/com/mateuszcer/basket/utils/io/FileParserTest.java
@@ -0,0 +1,45 @@
+package com.mateuszcer.basket.utils.io;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import java.util.Map;
+
+public class FileParserTest {
+
+    private FileParser fileParser;
+    @BeforeEach
+    public void setup() {
+        fileParser = new FileParser();
+    }
+
+    @Test
+    public void testLoadFromJSONFileList() {
+        // Given
+        String configFile = "src/test/resources/test-basket.json";
+
+        // When
+        List<String> data = fileParser.loadFromJSONFile(configFile);
+
+        // Then
+        assertNotNull(data);
+        assertEquals(data.size(), 4);
+        assertTrue(data.contains("Tart - Raisin And Pecan"));
+    }
+
+    @Test
+    public void testLoadFromJSONFileMap() {
+        // Given
+        String configFile = "src/test/resources/test-config.json";
+
+        // When
+        Map<String, List<String>> data = fileParser.loadFromJSONFile(configFile);
+
+        // Then
+        assertNotNull(data);
+        assertEquals(data.size(), 3);
+        assertTrue(data.containsKey("English Muffin"));
+    }
+}
